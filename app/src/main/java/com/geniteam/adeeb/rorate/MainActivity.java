d5345b6ecf5797f23c49e39af110562711639e71
Adeeb123456 <adeebpakistani@gmail.com>
2018-02-14 01:39:25 +0500

    nii

--- app/src/main/java/com/geniteam/adeeb/rorate/MainActivity.java
+++ app/src/main/java/com/geniteam/adeeb/rorate/MainActivity.java
@@ -1,9 +1,14 @@
 package com.geniteam.adeeb.rorate;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
@@ -11,6 +16,13 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private ImageView wheel;
     private double mCurrAngle = 0;
     private double mPrevAngle = 0;
+    float lastX=0;
+    float lastY=0;
+    private static Bitmap imageOriginal, imageScaled;
+    private static Matrix matrix;
+
+    private ImageView dialer;
+    private int dialerHeight, dialerWidth;
     ImageView bask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,42 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         wheel=(ImageView)findViewById(R.id.imageView1);
         wheel.setOnTouchListener(this);
 
+
+        //
+
+
+        // load the image only once
+        if (imageOriginal == null) {
+            imageOriginal = BitmapFactory.decodeResource(getResources(), R.drawable.wheel);
+        }
+
+        // initialize the matrix only once
+        if (matrix == null) {
+            matrix = new Matrix();
+        } else {
+            // not needed, you can also post the matrix immediately to restore the old state
+            matrix.reset();
+        }
+
+
+        dialer = (ImageView) findViewById(R.id.imageView1);
+     //   dialer.setOnTouchListener(new MyOnTouchListener());
+        dialer.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                // method called more than once, but the values only need to be initialized one time
+                if (dialerHeight == 0 || dialerWidth == 0) {
+                    dialerHeight = dialer.getHeight();
+                    dialerWidth = dialer.getWidth();
+
+                    // resize
+                    Matrix resize = new Matrix();
+                    resize.postScale((float)Math.min(dialerWidth, dialerHeight) / (float)imageOriginal.getWidth(), (float)Math.min(dialerWidth, dialerHeight) / (float)imageOriginal.getHeight());
+                    imageScaled = Bitmap.createBitmap(imageOriginal, 0, 0, imageOriginal.getWidth(), imageOriginal.getHeight(), resize, false);
+                }
+            }
+        });
     }
 
 
@@ -26,25 +74,30 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     public boolean onTouch(final View v, MotionEvent event) {
         final float xc = wheel.getWidth() / 2;
         final float yc = wheel.getHeight() / 2;
-
+       // Log.i("debug","img center x"+xc);
+      //  Log.i("debug","img center y"+yc);
         final float x = event.getX();
         final float y = event.getY();
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: {
-                wheel.clearAnimation();
+              //  wheel.clearAnimation();
                 mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
+                Log.i("debug","down angle "+mCurrAngle);
                 break;
             }
             case MotionEvent.ACTION_MOVE: {
                 mPrevAngle = mCurrAngle;
                 mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
+                if(mCurrAngle>-1)
                 animate(mPrevAngle, mCurrAngle, 0);
+                Log.i("debug","on move angle "+mCurrAngle);
+
                 System.out.println(mCurrAngle);
                 break;
             }
             case MotionEvent.ACTION_UP : {
-                mPrevAngle = mCurrAngle = 0;
+                mPrevAngle = mCurrAngle ;
                 break;
             }
         }
@@ -60,5 +113,70 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         rotate.setFillAfter(true);
         wheel.startAnimation(rotate);
         System.out.println(mCurrAngle);
+    }
+
+
+
+    private class MyOnTouchListener implements View.OnTouchListener {
+
+        private double startAngle;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            switch (event.getAction()) {
+
+                case MotionEvent.ACTION_DOWN:
+                    startAngle = getAngle(event.getX(), event.getY());
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    double currentAngle = getAngle(event.getX(), event.getY());
+                    rotateDialer((float) (startAngle - currentAngle));
+                    startAngle = currentAngle;
+                    break;
+
+                case MotionEvent.ACTION_UP:
+
+                    break;
+            }
+
+            return true;
+        }
+
+    }
+
+
+    private double getAngle(double xTouch, double yTouch) {
+        double x = xTouch - (dialerWidth / 2d);
+        double y = dialerHeight - yTouch - (dialerHeight / 2d);
+
+        switch (getQuadrant(x, y)) {
+            case 1:
+                return Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+            case 2:
+                return 180 - Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+            case 3:
+                return 180 + (-1 * Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI);
+            case 4:
+                return 360 + Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+            default:
+                return 0;
+        }
+    }
+
+
+    private static int getQuadrant(double x, double y) {
+        if (x >= 0) {
+            return y >= 0 ? 1 : 4;
+        } else {
+            return y >= 0 ? 2 : 3;
+        }
+    }
+
+
+    private void rotateDialer(float degrees) {
+        matrix.postRotate(degrees);
+        dialer.setImageBitmap(Bitmap.createBitmap(imageScaled, 0, 0, imageScaled.getWidth(), imageScaled.getHeight(), matrix, true));
     }
 }
